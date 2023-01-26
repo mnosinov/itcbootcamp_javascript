4135a557e463873c18bdac4f8aa09e064faafc67
mnosinov <mnosinov@gmail.com>
2023-01-26 19:36:31 +0600

    1st exercise

---
+++ app1.js
@@ -0,0 +1,23 @@
+let weight_in_kg = 74;
+let height_in_cm = 178;
+let current_reside_country = "Kazakhstan";
+let current_position = "Backend-Developer";
+let current_courses_in_progress = "Fullstack Developer (React.js, Node.js)";
+
+console.log(weight_in_kg);
+console.log(height_in_cm);
+console.log(current_reside_country);
+console.log(current_position);
+console.log(current_courses_in_progress);
+
+const firstName = "Mirbek";
+const lastName = "Nosinov";
+const middleName = "Maratovich";
+const birthPlace = "Kyrgyzstan";
+const gender = "Male";
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(birthPlace);
+console.log(gender);
