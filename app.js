38399e0848dd1b5507164404eb163e8d63e09dd4
mnosinov <mnosinov@gmail.com>
2023-02-10 20:26:05 +0600

    js complex sorting

--- app.js
+++ app.js
@@ -26,15 +26,10 @@ const mixed = [5, "фdog", 1, "яcat", 9, "цbird", 4, "шlion", 6, "дelephant"
 
 const mixSorting = function(elements) {
 	return elements.sort( (a, b) => {
-		if (typeof a === 'number' && typeof b === 'number') {
-			return a - b;
-		} else if (typeof a === 'string' && typeof b === 'string') {
-			return b.localeCompare(a);
-		} else if (typeof a === 'number' && typeof b === 'string') {
-			return 1;
-		} else if (typeof a === 'string' && typeof b === 'number') {
-			return -1;
-		}
+		if (typeof a === 'number' && typeof b === 'number') return a - b;
+		else if (typeof a === 'string' && typeof b === 'string') return b.localeCompare(a);
+		else if (typeof a === 'number' && typeof b === 'string') return 1;
+		else if (typeof a === 'string' && typeof b === 'number') return -1;
 	});
 }
 
